7763df784b6b33e98517fdb3670349537e3c9418
Oskar Westberg <oskar.isidor.gustafsson@gmail.com>
2025-05-16 09:17:00 +0200

    Don't run admission webhooks in dev environemnt

--- cmd/main.go
+++ cmd/main.go
@@ -153,11 +153,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&cloudamqpcomv1alpha1.LavinMQ{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "LavinMQ")
-		os.Exit(1)
+	if os.Getenv("ENABLE_WEBHOOKS") == "true" {
+		if err = (&cloudamqpcomv1alpha1.LavinMQ{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "LavinMQ")
+			os.Exit(1)
+		}
 	}
-
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
